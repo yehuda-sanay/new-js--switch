2580d59f555064fabd6122582670bfa1ebd3d286
yehuda-sanay <yehudasanay1@gmail.com>
2022-06-16 09:22:32 +0300

    16.06.22

---
+++ main.js
@@ -0,0 +1,38 @@
+// function getNumberFromUserAndPrint(){
+//     const user_number= +prompt();
+//  switch (user_number){
+//     case 3:
+//         console.log("שלום");
+//     break;
+//     case 5:
+//         console.log("להתראות");
+//     break;
+//     case 7:
+//         console.log("תודה");
+//     break;
+//     default:
+//         console.log("אין הודעה מתאימה");
+
+//  }
+// }
+// getNumberFromUserAndPrint()
+
+// function getNameFromUserAndPrint(){
+//     const user_name=prompt();
+//     switch(user_name){
+//         case "jacob":
+//             document.write(user_name);
+//         break;
+//         case "nathan":
+//             document.write(user_name.toLocaleUpperCase());
+//         break;
+//         case "DALYA":
+//             document.write(user_name.toLowerCase());
+//             break;
+//         default:
+//             document.write("אין הודעה מתאימה");
+//     }
+// }
+// getNameFromUserAndPrint()
+
+
