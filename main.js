e0f1e8de732c27d3e5e8592e2bd25bd7852fa013
yehuda-sanay <yehudasanay1@gmail.com>
2022-06-28 01:30:47 +0300

    28.06.22

--- main.js
+++ main.js
@@ -35,4 +35,69 @@
 // }
 // getNameFromUserAndPrint()
 
+// function doCalculation(){
+//     switch(selct_id.value){
+//         case "+":
+//             console.log(first.value+secend.value);
+//             break;
+//         case "-":
+//             console.log(first.value-secend.value);
+//             break;
+//         case "*":
+//             console.log(first.value*secend.value);
+//             break;
+//         case "/":
+//             console.log(first.value/secend.value);
+//             break;
+//         default:
+
+//     }
+// }
+
+// function printTextColorSize(){
+//     switch(select_id.value){
+//         case "h1":
+//             div_id.innerHTML=`<h1 style=color:#${user_color.value}>${user_text.value}</h1>`;
+//             break;
+//         case "h2":
+//             div_id.innerHTML=`<h2 style=color:#${user_color}>${user_text.value}</h2>`;
+//             break;
+//         case "h3":
+//             div_id.innerHTML=`<h3 style=color:#${user_color}>${user_text.value}</h3>`;
+//             break;
+//         case "h4":
+//             div_id.innerHTML=`<h4 style=color:#${user_color}>${user_text.value}</h4>`;
+//             break;
+//         case "h5":
+//             div_id.innerHTML=`<h5 style=color:#${user_color}>${user_text.value}</h5>`;
+//             break;
+//         case "h6":
+//             div_id.innerHTML=`<h6 style=color:#${user_color}>${user_text.value}</h6>`;
+//             break;
+
+        
+//         }
+    
+// }
+
+function movCircleColor(){
+    switch(placae.value){
+        case "left_top":
+            
+        break;
+        case "right_top":
+
+        break;
+        case "right_down":
+
+        break;
+        case "left_down":
+        
+        break;
+
+        default:
+
+    }
+}
+
 
